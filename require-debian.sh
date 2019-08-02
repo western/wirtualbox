e4f5c3b41c23300c9e89701519c2cd3f756a9fcd
Andrey (muz) <westroads@gmail.com>
2019-08-02 11:19:25 +0300

    add required scripts

---
+++ require-debian.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# check system libs for required
+
+if ! whoami | grep -q root; then
+    #echo 'root required. exit.'
+    #exit 1
+fi
+
+if cat /etc/*release* | grep -q 'VERSION="9 (stretch)"'; then
+    echo "Debian 9 found"
+else
+    echo "Debian 9 not found"
+    exit 2
+fi
+
---
+++ require-opensuse.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# check system libs for required
+
+if ! whoami | grep -q root; then
+    #echo 'root required. exit.'
+    #exit 1
+fi
+
+if cat /etc/*release* | grep -q 'openSUSE Leap 15.1'; then
+    echo 'openSUSE Leap 15.1 found'
+else
+    echo 'openSUSE Leap 15.1 required! exit.'
+    exit 2
+fi
+
+
