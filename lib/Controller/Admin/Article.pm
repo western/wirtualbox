bbd689e1bbefe11d0c3f5c472fa0ad8b6bb5f6ee
Andrey (muz) <westroads@protonmail.com>
2020-01-29 08:03:56 +0300

    clear code

--- lib/Controller/Admin/Article.pm
+++ lib/Controller/Admin/Article.pm
@@ -16,46 +16,7 @@ sub index {
     my($self, $r, $args) = @_;
     
     
-    #die dumper $r->model->db;
-    #die dumper $r->model->Article->db;
-    #die dumper $r->model->Article->list;
-    #die $r->model->Article->list->[0]->{registered}->value;
     
-    #$r->model->Article->join( 'users' )->list->[0];
-    #$r->model->Article->join( 'left users' )->list->[0];
-    #$r->model->Article->join( 'left users' => 'articles.user_id = users.xx' )->list->[0];
-    #$r->model->Article->join( 'comments' )->list->[0];
-    #$r->model->Article->join( 'left comments' )->list->[0];
-    
-    #die dumper $r->model->Article->join( 'users' )->list()->[0];
-    #die dumper $r->model->Article->join( 'left users' )->list()->[0];
-    #die dumper $r->model->Article->join( 'comments' )->list()->[0];
-    
-    #my $all = $r->model->Article->join( 'comments' )->list();
-    #die dumper $all;
-    
-    #my $row = $r->model->Article->join( 'comments' )->list()->[0];
-    #die dumper $row->{body};
-    
-    #my $row = $r->model->Article->select('users.login')->join( 'users' )->join( 'comments' )->list->[0];
-    #my $row = $r->model->Article->join( 'users' )->join( 'comments' )->list->[0];
-    #die dumper $row;
-    
-    #my $row = $r->model->Article->join( 'users' )->join( 'comments' )->list( -data=>1 )->[0];
-    #my $all = $r->model->Article->join( 'users' )->join( 'comments' )->list( -data=>1 );
-    #die dumper $all;
-    
-    #my $row = $r->model->Article->join( 'left users' )->list( -data=>1 )->[0];
-    #die dumper $row;
-    
-    #my $list = $r->model->Article->join( 'left users' )->gain('comments')->limit(3)->offset(0)->list( -data=>1 );
-    #my $row = $r->model->Article->join( 'left users' )->gain('users')->limit(1)->offset(0)->list( -data=>1 )->[0];
-    #die dumper $list;
-    
-    #$r->response->json({
-    #    code => 'ok',
-    #    row  => $row,
-    #});
     
     my $filter_page = $r->param('filter_page') || 1;
     $filter_page = int $filter_page;
@@ -66,7 +27,7 @@ sub index {
     my $list_count = $r->model->Article->join('left user')->count;
     
     
-    #die dumper $list->[0];
+    
     
     
     my $list_pages = $list_count / $list_limit;
@@ -90,16 +51,7 @@ sub index {
 sub edit {
     my($self, $r, $args) = @_;
     
-    #die dumper $args;
     
-    #my $data = $r->model->Article->where(id => $args->{id})->first;
-    #my $data = $r->model->Article->where(id => $args->{id}, title => 'title1')->first;
-    #my $data = $r->model->Article->where('id = ? and title like ?', $args->{id}, 'title%')->first;
-    
-    #die $data->{body}->value;
-    #die dumper $data;
-    
-    #my $data = $r->model->Article->where(id => $args->{id})->attach('regions', 'users')->first( -flat=>1, -json=>1 );
     my $data = $r->model->Article->where(id => $args->{id})->list( -flat=>1, -json=>1 )->[0];
     
     if( !$data ){
@@ -116,6 +68,7 @@ sub edit {
     $r->response->template_args(
         data        => $data,
         region_list => JSON::XS->new->utf8->encode($region_list),
+        h1_title    => 'Article edit',
     );
 }
 
@@ -129,6 +82,7 @@ sub new {
     $r->response->template_args(
         
         region_list => JSON::XS->new->utf8->encode($region_list),
+        h1_title    => 'Article new',
     );
 }
 
